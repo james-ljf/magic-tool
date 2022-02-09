b8030f3ec01bdb7a2fc710283fb20817da6e55f4
tangxin <tangxin@jiaxincloud.com>
2022-02-09 21:13:39 +0800

    Mybatis plus 版本分页兼容修改

--- src/main/java/com/magictool/web/configuration/MybatisPlusPageConfig.java
+++ src/main/java/com/magictool/web/configuration/MybatisPlusPageConfig.java
@@ -2,13 +2,8 @@ package com.magictool.web.configuration;
 
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
-<<<<<<< Updated upstream
-import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-=======
-import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
->>>>>>> Stashed changes
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,25 +12,8 @@ import org.springframework.context.annotation.Configuration;
  * @author lijf
  */
 @Configuration
-<<<<<<< Updated upstream
 @ConditionalOnClass(value = {PaginationInnerInterceptor.class})
 public class MybatisPlusPageConfig {
-
-    /**
-     *  旧版
-=======
-public class MybatisPlusPageConfig {
-
-    /**
-     *  最新版
->>>>>>> Stashed changes
-     */
-    @Bean
-    public MybatisPlusInterceptor mybatisPlusInterceptor() {
-        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.H2));
-        return interceptor;
-    }
 
     /**
      *  最新版
